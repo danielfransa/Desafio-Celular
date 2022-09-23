8eae5d7e09a73b47f358ff7cad8935aa0e87a3f8
danielfransa <daniel_fransa@hotmail.com>
2022-09-23 10:33:29 -0300

    Entraga Projeto

---
+++ Models/Smartphone.cs
@@ -0,0 +1,34 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string Modelo { get; set; }
+        protected string Imei { get; set; }
+        protected int Memoria { get; set; }
+
+        public Smartphone(string numero, string modelo,  string imei,  int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "19982342134", modelo: "N-22",imei: "228737636273836", memoria: 256 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "19982215674", modelo: "Iphone 14 Pro",imei: "228737636273836", memoria: 512 );
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
